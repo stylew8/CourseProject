99b7d2912e7c58de40ade1ce6990925a9f78a2dc
stylew8 <vaniasavochkin@gmail.com>
2025-02-24 13:06:44 +0200

    deploy

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://uniqum.school/api';
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL || API_BASE_URL,
