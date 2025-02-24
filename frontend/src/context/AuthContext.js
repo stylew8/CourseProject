a815049e4d5f00a47a76677474c1217422efa8d5
stylew8 <vaniasavochkin@gmail.com>
2025-02-24 15:57:03 +0200

    inst

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { axiosInstance } from '../api/axiosInstance'
+import axiosInstance from '../api/axiosInstance'
 
 export const AuthContext = createContext();
 
